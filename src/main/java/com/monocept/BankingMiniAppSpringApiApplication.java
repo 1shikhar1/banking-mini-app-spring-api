741b8bc5648e9b394e01a3f9a3979a9fe59072ec
1shikhar1 <1shikhar1@gmail.com>
2022-05-22 22:42:51 +0530

    Update

--- src/main/java/com/monocept/BankingMiniAppSpringApiApplication.java
+++ src/main/java/com/monocept/BankingMiniAppSpringApiApplication.java
@@ -1,4 +1,4 @@
-package com.monocept.bankingminiappspringapi;
+package com.monocept;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
