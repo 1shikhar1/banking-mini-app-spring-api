988aa30b516d6a4f18e1c2d6b18752e1de37b8e0
rohan sahu <rohansahu1212@gmail.com>
2022-05-23 00:29:34 +0530

    controller added

--- src/main/java/com/monocept/controller/BankController.java
+++ src/main/java/com/monocept/controller/BankController.java
@@ -1,35 +1,46 @@
 package com.monocept.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.monocept.model.Account;
+import com.monocept.service.AccountService;
 
 @RestController
 @RequestMapping("/api/v1/bank")
 public class BankController {
+	@Autowired
+	AccountService service;
 
 	public BankController() {
-	
-	}
-	
 
-	@GetMapping
-	public String get() {
-		return "inside get";
 	}
 
-	@PostMapping
-	public String post() {
-		return "inside post";
+	@GetMapping(path = "/accounts")
+	public ResponseEntity<List<Account>> getAllAccounts() {
+		return ResponseEntity.ok(service.getAccounts());
+	}
+
+	@PostMapping(path = "/addAccount")
+	public ResponseEntity<Account> post(@RequestBody Account account) {
+		service.addAccount(account);
+		return ResponseEntity.ok(account);
 	}
 
 	@PutMapping
 	public String put() {
 		return "inside put";
 	}
+
 	@DeleteMapping
 	public String delete() {
 		return "inside delete";
