b9fe9ffe42e9885abeafa3e5d41f339d0eab0ef9
1shikhar1 <1shikhar1@gmail.com>
2022-05-22 22:45:36 +0530

    update

--- src/main/java/com/monocept/Application.java
+++ src/main/java/com/monocept/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankingMiniAppSpringApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankingMiniAppSpringApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
