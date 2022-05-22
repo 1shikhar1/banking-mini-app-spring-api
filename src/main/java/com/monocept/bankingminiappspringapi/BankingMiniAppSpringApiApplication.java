0dd4a88fcf9527213880c31ad3ce02925b4e9440
1shikhar1 <1shikhar1@gmail.com>
2022-05-22 22:33:21 +0530

    update

---
+++ src/main/java/com/monocept/bankingminiappspringapi/BankingMiniAppSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.monocept.bankingminiappspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingMiniAppSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingMiniAppSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monocept/bankingminiappspringapi/BankingMiniAppSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monocept.bankingminiappspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingMiniAppSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
